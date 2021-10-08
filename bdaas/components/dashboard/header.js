886396dbb452eb358938b7102a120b3d7794ef9e
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-10-08 20:01:01 +0100

    create dashboard for adding birthdays

---
+++ bdaas/components/dashboard/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h1 class="text-2xl p-2 border-b">BDaaS</h1>
+    </div>
+  );
+}
---
+++ bdaas/pages/dashboard.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import { supabase } from "../utils/supabaseClient";
+import Header from "../components/dashboard/header";
+import NewBirthday from "../components/dashboard/newBirthday";
+
+export default function Dashboard() {
+  const [session, setSession] = useState(null);
+  const [newBirthdayOpen, setNewBirthdayOpen] = useState(false);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <div>
+        Howdy {session ? session.user.user_metadata.full_name : "not logged in"}
+      </div>
+      <button onClick={() => setNewBirthdayOpen(!newBirthdayOpen)}>
+        Add birthday
+      </button>
+      {newBirthdayOpen ? <NewBirthday /> : ""}
+    </div>
+  );
+}
