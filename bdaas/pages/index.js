10bfa77ba33442dea3b51dfa7a802c6463ad6ed9
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-10-08 18:28:23 +0100

    create login button

--- bdaas/pages/index.js
+++ bdaas/pages/index.js
@@ -1,19 +1,35 @@
-import Image from "next/image";
+import { useState } from 'react'
+import { supabase } from '../utils/supabaseClient'
 
 export default function IndexPage() {
-  return (
-    <div className="flex flex-col items-center justify-center space-y-12">
-      <Image
-        src="/team-of-critters.svg"
-        alt="Four one-eyed aliens playing"
-        width={576}
-        height={429.734}
-        priority
-      />
+  const [loading, setLoading] = useState(false)
 
-      <h2 className="p-3 font-bold bg-yellow-300 md:text-2xl">
-        Hi! Welcome to your first Next.js site.
-      </h2>
+  const handleLogin = async () => {
+    try {
+      setLoading(true);
+      const { error } = await supabase.auth.signIn({ provider: 'google' });
+      if (error) throw error;
+    } catch (error) {
+      alert(error.error_description || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            handleLogin();
+          }}
+          className=""
+          disabled={loading}
+        >
+          <span>{loading ? "Loading" : "Sign in with Google"}</span>
+        </button>
+      </div>
     </div>
   );
 }
