79e41e2bb90ba5023a2f72de2fc15e3ed6aef9e8
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-10-09 16:21:08 +0100

    frontend: set max-width for dashboard body and style add birthday button

--- bdaas/pages/dashboard.js
+++ bdaas/pages/dashboard.js
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../utils/supabaseClient";
+import Container from "../components/container";
 import Header from "../components/dashboard/header";
 import NewBirthday from "../components/dashboard/newBirthday";
 import Birthdays from "../components/dashboard/birthdays";
+
+// TODO: Add icons to buttons
 
 export default function Dashboard() {
   const [session, setSession] = useState(null);
@@ -19,14 +22,22 @@ export default function Dashboard() {
   return (
     <div>
       <Header />
-      <div>
-        Howdy {session ? session.user.user_metadata.full_name : "not logged in"}
-      </div>
-      <button onClick={() => setNewBirthdayOpen(!newBirthdayOpen)}>
-        Add birthday
-      </button>
-      {newBirthdayOpen ? <NewBirthday /> : ""}
-      <Birthdays />
+      <Container>
+        <div class="mt-8">
+          <div>
+            Howdy{" "}
+            {session ? session.user.user_metadata.full_name : "not logged in"}
+          </div>
+          <button
+            class="bg-gray-900 hover:bg-gray-800 text-white px-4 py-2"
+            onClick={() => setNewBirthdayOpen(!newBirthdayOpen)}
+          >
+            Add birthday
+          </button>
+          {newBirthdayOpen ? <NewBirthday /> : ""}
+          <Birthdays />
+        </div>
+      </Container>
     </div>
   );
 }
