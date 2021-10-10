f5714a7c09ab5244c380a981173d3e35c96d5153
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-10-10 20:43:21 +0100

    frontend: add meta tags

--- bdaas/pages/_app.js
+++ bdaas/pages/_app.js
@@ -5,10 +5,10 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Next.js Starter Tailwind</title>
+        <title>Birthday email reminders - Birthdays as a Service</title>
         <meta
           name="Description"
-          content="A Next.js starter styled using Tailwind CSS."
+          content="BDaaS makes sure you never forget a friends birthday. When you addra person's birthday on your dashboard, you will receive a timely email notification on the morning of their birthday."
         />
       </Head>
 
