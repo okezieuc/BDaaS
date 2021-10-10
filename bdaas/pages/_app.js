a72779ac955275d4209ef8ab97660353767ff272
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-10-10 20:45:47 +0100

    frontend: minor meta tag fix

--- bdaas/pages/_app.js
+++ bdaas/pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Birthday email reminders - Birthdays as a Service</title>
+        <title>Birthday Email Reminders - Birthdays as a Service</title>
         <meta
           name="Description"
           content="BDaaS makes sure you never forget a friends birthday. When you addra person's birthday on your dashboard, you will receive a timely email notification on the morning of their birthday."
